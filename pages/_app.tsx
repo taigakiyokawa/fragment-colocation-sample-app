c06e555e6f8474fc17ffd749da08cfeb85bead90
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2022-12-10 16:32:34 +0900

    [fix] Follow ts strict

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import { ApolloProvider } from '@apollo/client'
+import { AppProps } from 'next/app'
 import { useApollo } from '../apollo/client'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState)
 
   return (
