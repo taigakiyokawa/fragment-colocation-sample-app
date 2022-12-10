340fb418e2f2bcc54a259e36af3016038c6d86c7
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2022-12-10 17:19:28 +0900

    [feat] Change codegen config for colocating genereated types

--- codegen.ts
+++ codegen.ts
@@ -3,13 +3,19 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 const config: CodegenConfig = {
   schema: 'graphql/type-defs.ts',
   generates: {
-    'graphql/__generated__/graphql-types.ts': {
-      documents: 'graphql/**/*.graphql',
-      plugins: [
-        'typescript',
-        'typescript-operations',
-        'typescript-react-apollo',
-      ],
+    'graphql/__generated__/graphql-schema-types.ts': {
+      plugins: ['typescript'],
+    },
+    'components/': {
+      documents: 'components/**/index.tsx',
+      preset: 'near-operation-file',
+      plugins: ['typescript-operations', 'typescript-react-apollo'],
+      presetConfig: {
+        baseTypesPath: '../graphql/__generated__/graphql-schema-types.ts',
+        folder: '__generated__',
+        extension: '.ts',
+        importTypesNamespace: 'SchemaTypes',
+      },
     },
   },
   config: { avoidOptionals: true },
