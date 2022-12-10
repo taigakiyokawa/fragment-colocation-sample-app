e0c085c7ab01dbc8b415055e4a8c4e29de17da30
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2022-12-10 20:38:18 +0900

    [chore] Add `styles/globals.css` with `@tailwind` directives

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ApolloProvider } from '@apollo/client'
 import { AppProps } from 'next/app'
 import { useApollo } from '../apollo/client'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState)
